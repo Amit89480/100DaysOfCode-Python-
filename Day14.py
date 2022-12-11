95788f90546bdfa1284c5994bcb9e9cfb14c751c
Amit pandey <pamit7407084@gmail.com>
2022-12-11 22:29:50 +0530

    Day14

--- Day14.py
+++ Day14.py
@@ -1,1 +1,47 @@
 # In this we will be learning conditional statements
+# first If-else
+
+# a=100
+# if a>100:
+#     print("Less")
+#
+# else:
+#     print("more")
+
+
+# if-else-elif
+#
+# age=int(input("Enter your age: "))
+# if(age<18):
+#     print("you cannot drive!")
+# elif(age>18):
+#     print("you can drive")
+# else:
+#     print("Go make your license first!")
+
+
+# nested if-else-elif
+
+# amount=int(input("Enter the amount you have in your pocket: "))
+# if(amount>500):
+#     print("buy pizza")
+# elif(amount<500):
+#     print("buy panner butter masala")
+# elif(amount<400):
+#     print("eat chola batura")
+# else:
+#     print("Go home")
+
+
+# if else ladder
+#
+# amount=int(input("Enter the amount you have: "))
+# if(amount<=1000):
+#     print("Enjoy you are so rich")
+# elif(amount<900):
+#     if(amount>900):
+#         print("Go to manali")
+#     else:
+#         print("Go home")
+# else:
+#     print("GO enjoy buddy")
