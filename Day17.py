8275bcd045c00db3fbc23c29e19beb549f42561e
Amit pandey <pamit7407084@gmail.com>
2022-12-14 22:29:12 +0530

    Day 17

---
+++ Day17.py
@@ -0,0 +1,18 @@
+# In this we have learned about the for Loop
+
+# list = ["Amit","Ashis","Misti"]
+# for i in list:
+#     print(i)
+#     for j in i:
+#         print(j)
+
+
+# here we have learned about the range function
+# for i in range(1,5):
+#     print(i)
+
+
+# here tha last argument depicts the increment and decrement
+
+# for i in range(1,10,3):
+#     print(i)
