5253e61a8e8adbe6b657ec77919d1b2101900a53
Amit pandey <pamit7407084@gmail.com>
2022-12-15 22:51:02 +0530

    Day 18

---
+++ Day18.py
@@ -0,0 +1,19 @@
+# In this we have learned about while loops and do while loops
+
+# n = int(input("Enter the Number: "))
+#
+# while n>=0:
+#     print(n)
+#     n=n-1
+
+
+
+
+# Here we have written this program to enumeretate do-while loop
+
+# n=int(input("number "))
+# print(n);
+# while(n<=1):
+#     n=int(input("Enter the number to enumerate do-while loop"))
+#     print(n);
+
