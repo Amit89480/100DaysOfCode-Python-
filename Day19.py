300a6bf2b61c502fb2ed611ff0ce10805f2f4309
Amit pandey <pamit7407084@gmail.com>
2022-12-16 23:32:56 +0530

    Day19

---
+++ Day19.py
@@ -0,0 +1,19 @@
+# In this we will be learning about the continue and break statement
+
+
+# using this below code we can also demonstrate the do-while loop
+# n = int(input("Enter the number: "))
+# while True:
+#     print("Hello Hi Bye Bye")
+#     if(n%2==0):
+#         break
+
+
+
+# The python interpreter executes the code line by line
+
+# for i in range(1, 11):
+#     if (i == 5):
+#         continue
+#     print("2 x ", i, "=", 2 * i)
+
