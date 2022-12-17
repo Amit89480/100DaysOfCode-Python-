2d039650ecaea1f086358b17c49c119c99d22155
Amit pandey <pamit7407084@gmail.com>
2022-12-17 21:54:15 +0530

    Day 20

---
+++ Day20.py
@@ -0,0 +1,37 @@
+# In this we will be learning about the functions and seeing different things about function
+
+
+
+
+# Function for summing up to numbers
+
+# def sum(a, b):
+#     return a + b
+# 
+# 
+# print(sum(23, 78))
+
+
+# suppose a programmer wants to write a programmer later so he can write pass keyword so that python interpreter 
+# doesnt generate error 
+# def fibonacci(a): pass 
+
+
+# program for fibonacci series
+
+
+# def fibo(n):
+#     num1=0
+#     num2=1
+#     for i in range(n):
+#         ans=num1+num2
+#         num1=num2
+#         num2=ans
+#     return ans
+#
+#
+# print(fibo(12), end =" ")
+
+
+
+ 
