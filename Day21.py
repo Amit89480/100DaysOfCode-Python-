79a1415044d4f715fd710681817286537010e16e
Amit pandey <pamit7407084@gmail.com>
2022-12-18 23:27:11 +0530

    Day 21

---
+++ Day21.py
@@ -0,0 +1,51 @@
+"""IN this we have learned about the type of function Arguments:
+Default arguments
+variable length arguments
+keyword arguments
+required arguments"""
+
+
+# Default Arguments
+
+# def add(a=4,b=90):
+#     print(a+b)
+#
+# add(6)
+
+
+# KeyWord Arguments
+
+# def add(a , b):
+#     print(a+b)
+#
+# add(b=90,a=67)
+
+
+# **************************** Variable length Arguments***********************
+
+# *************************Arbitary Arguments************************
+
+# def name(*a):
+#      print("hey", a[0], a[1], a[2])
+#
+#
+# name("ashis", "misti", "amit")
+
+
+# ****************************keyword arbitary arguments*********************
+
+# def name(**a):
+#     print("Hello,", a["name1"], a["name2"], a["name3"])
+#
+#
+# name(name1="Amit", name2="Misti", name3="Bhebhe")
+
+
+# Return Statement
+
+# def add(a, b):
+#     return a - b  # First return statement will execute
+#     return a + b
+#
+#
+# print(add(90, 10))
