7b84326868bbfb84c1bf3fd297ae5fcdfbd1285c
Amit pandey <pamit7407084@gmail.com>
2022-12-26 23:08:55 +0530

    Day 29

---
+++ Day29.py
@@ -0,0 +1,19 @@
+# In this we will be learning about the Docstring and PEP 8
+
+
+# Here we are savong he docstring it is different from comments
+# Below is the zen of python
+import this
+
+
+def add(a,b):
+    '''In this we will be performing add operations'''
+    print(a+b)
+
+add(67,89)
+
+print(add.__doc__)
+
+
+
+# Use Import this for printing the poem of zen of python by tim peters
