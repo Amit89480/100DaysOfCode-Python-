a21a5397898bcc0d5f9fd2ba631f9f3bf33f8685
Amit pandey <pamit7407084@gmail.com>
2022-12-29 00:45:09 +0530

    Day 30 done

---
+++ Day31.py
@@ -0,0 +1,17 @@
+# In this we have learned about the Sets
+
+
+set1={"amit",4,4,5,6,7}
+
+for values in set1:
+    print(values)
+print(set1)
+
+
+# Set is enchangeable so we cannot change the set elements
+
+# Creating an empty set
+
+set2=set();
+
+print(type(set2))
