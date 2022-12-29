b656b4b352bdc5fe58b0ebebbc107465d4e03cd2
Amit pandey <pamit7407084@gmail.com>
2022-12-30 00:59:33 +0530

    Day 32 done

---
+++ Day32.py
@@ -0,0 +1,65 @@
+# In this we will be learning about the sets
+
+set1={6,7,8,4,5}
+set2={6,7,8,4,5,67,89,34}
+
+# Here we will be doing union
+#
+# print(set1.union(set2))
+# set1.update(set2)
+# print(set1)
+
+
+# print(set1.intersection(set2))
+
+# set1.intersection_update(set2)
+# print(set1)
+
+
+# print(set1.symmetric_difference(set2))
+
+#A-B WILL PRINT DIFFERENT ELEMENT PRESENT IN A AND VICE VERSA
+
+# print(set2.difference(set1))
+# set2.difference_update(set1)
+#
+# print(set2)
+
+
+# print(set1.isdisjoint(set2))
+# print(set1.issuperset(set2))
+
+
+# print(set1.issubset(set2))
+
+
+# set1.add("Amit")
+# print(set1)
+
+
+# set1.update("amit","misti")
+# print(set1)
+
+
+
+# set1.remove("amit")->will throw error if not present
+
+# set1.discard("amit")
+
+
+# set1.pop()->will pop any element without order
+
+# del set1
+# print(set1)
+
+
+# set1.clear()
+# print(set1)
+
+
+# if 32 in set1:
+#     print("Yes")
+# else:
+#     print("No")
+
+
