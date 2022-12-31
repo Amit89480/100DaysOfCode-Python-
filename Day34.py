6c038c0e2fb4bc7e7e059c52f5db7c95756293cc
Amit pandey <pamit7407084@gmail.com>
2022-12-31 23:53:34 +0530

    Day 34  done

---
+++ Day34.py
@@ -0,0 +1,42 @@
+# In this we will be learning about the different dictionary methods
+
+
+# creating an empty dictionary
+
+dict = {}
+print(dict)
+
+
+# creating a dictionary
+dict1 = {"Amit":89,"Ashis":90,"Misti":56}
+
+
+# to update the dictionary
+update_dict={"bhebhe":91}
+dict.update(update_dict)
+print(dict)
+
+# to clear the dictionary
+# dict.clear();
+# print(dict)
+
+# to pop the item by using key number
+dict.pop("bhebhe")
+# print(dict)
+
+
+# to pop the item from last
+# dict.popitem()
+# print(dict)
+
+# to delete entire dictionary
+
+# del dict1
+# print(dict1)
+
+
+
+# to Delete with key
+
+# del dict1["Misti"]
+# print(dict1)
