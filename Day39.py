e3704f5176063807590df5e3b7957bbbc688a3fc
Amit pandey <pamit7407084@gmail.com>
2023-01-13 14:34:31 +0530

    Day 39 yet to solve

---
+++ Day39.py
@@ -0,0 +1,17 @@
+questions = [
+    ["Who invented C?","Denis Ritechie"],
+    ["Who invented C?","Denis Ritechie"],
+    ["Who invented C?","Denis Ritechie"],
+    ["Who invented C?","Denis Ritechie"]
+
+]
+
+
+
+levels = [1000,2000,3000,4000]
+
+
+# Will continue for here
+
+
+
