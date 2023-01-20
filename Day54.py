e692d28ed79eb7b7e749d1d36134f567f05c77f4
Amit pandey <pamit7407084@gmail.com>
2023-01-20 22:25:30 +0530

    Day 54

---
+++ Day54.py
@@ -0,0 +1,9 @@
+# Here we will be learning about 'is' keyword vs '==' operator
+
+# a=4
+# b=4
+# As is checks the exact location of object in a memory
+a = [1,2,3]
+b = [1,2,3]
+print(a is b)
+print(a == b)
