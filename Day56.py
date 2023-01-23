ee4966cd5e6805eda09e624ef304548262cf36fb
Amit pandey <pamit7407084@gmail.com>
2023-01-23 22:18:52 +0530

    Day 56 done

---
+++ Day56.py
@@ -0,0 +1,9 @@
+# In this we have seen about what is oops and what is class and object and how they are works
+
+
+# Also we have seen that what are the key features of OOPS
+
+# 1. Polymorphism
+# 2.encapsulation
+# 3.abstraction
+# 4.inheritance
