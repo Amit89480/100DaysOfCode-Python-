30f16e2f43f6037727956ec896cc1a34383b280b
Amit pandey <pamit7407084@gmail.com>
2023-02-03 00:26:10 +0530

    Day 64 Question solved

--- Day64.py
+++ Day64.py
@@ -1,1 +1,24 @@
 # Here we will be solving the program of library management system
+
+class Library:
+    def __init__(self):
+        self.books = []
+        self.noOfBooks = 0
+
+    def addBooks(self, books):
+        self.books.append(books)
+        self.noOfBooks = len(self.books)
+
+    def showInfo(self):
+        print(f"The Library has {self.noOfBooks} Books. The books are:: ")
+        for book in self.books:
+            print(book)
+
+
+l1 = Library()
+l1.addBooks("Web development")
+l1.addBooks("Node js")
+l1.addBooks("Javascript")
+l1.addBooks("TensorFlow")
+l1.addBooks("SykitLearn")
+l1.showInfo()
