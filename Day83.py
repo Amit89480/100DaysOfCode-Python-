ee3546e22ab2ee54f5475e4334f1b73aa3509fcf
Amit pandey <pamit7407084@gmail.com>
2023-02-24 00:24:47 +0530

    Day 83 done

--- Day83.py
+++ Day83.py
@@ -1,1 +1,16 @@
 # In this we have write a code for pronouncing the name from list using win32package
+
+
+import pyttsx3
+# initialize Text-to-speech engine
+engine = pyttsx3.init()
+# convert this text to speech
+text = ["Amit","AAmit","Misti","Ashis","Rohan"]
+for name in text:
+    engine.say(f"Shoutout to {name}")
+    engine.runAndWait()
+
+
+
+
+
